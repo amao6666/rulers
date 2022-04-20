9342fc7e3152c05e82ef6a9248c2f98e625db8a6
amao6666 <17835268768@163.com>
2022-04-20 19:57:36 +0800

    add Application Class

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,6 +1,11 @@
 require "rulers/version"
 
 module Rulers
-  class Error < StandardError; end
-  # Your code goes here...
+  class Application
+    def call(env)
+      p '='*80
+      p env
+      [200, {'Content-Type' => 'text/html'}, ['Hello, world']]
+    end
+  end
 end
